d93da42fb2910a669184ef355d25fe7244f3cd9a
Gustavo Buratto <gustavo_buratto@hotmail.com>
2024-10-12 11:19:23 -0300

    Cria o model 'Cliente'

---
+++ src/app/models/cliente.spec.ts
@@ -0,0 +1,25 @@
+import { Cliente } from './cliente';
+
+describe('Cliente', () => {
+
+  let cliente: Cliente;
+
+  beforeEach(() => {
+    cliente = new Cliente(
+      'José Pereira',
+      100.00,
+      new Date('2024-10-12T11:12:10.000Z'),
+    );
+  });
+
+  describe('Cliente', () => {
+    it('should create an instance', () => {
+      expect(cliente).toBeTruthy();
+    });
+  });
+
+  it('deveria conter "José Pereira" como nome', () => {
+    expect(cliente.nome).toBe('José Pereira');
+  });
+
+});
