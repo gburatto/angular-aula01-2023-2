e909f84547f250a06cc8f53614c8b55390221604
Gustavo Buratto <gustavo_buratto@hotmail.com>
2024-10-13 09:28:58 -0300

    Introduz cores para saldo positivo/negativo (via método).

--- src/app/components/consulta-saldo/consulta-saldo.component.ts
+++ src/app/components/consulta-saldo/consulta-saldo.component.ts
@@ -12,10 +12,26 @@ import { CommonModule } from '@angular/common';
   styleUrl: './consulta-saldo.component.css'
 })
 export class ConsultaSaldoComponent {
+
   public cliente: Cliente = new Cliente(
     'Maria das Couves',
     99.99,
     new Date(),
   );
+
+  /**
+   * Retorna `'zero'`, `'positivo'` ou `'negativo'`, de acordo com o número especificado.
+   *
+   * @param valor Número a ser verificado.
+   */
+  public getClassNamePorValor(valor: number): string {
+    if (valor >= 0.005) {
+      return 'positivo';
+    } else if (valor > -0.005 && valor < 0.005) {
+      return 'zero';
+    } else {
+      return 'negativo';
+    }
+  }
 };
 
